c8dac77c179df1bda6009d9fb451efc8e7ca5a5b
Emediong Eshiet <eshietemedionguyobong@gmail.com>
2025-02-20 10:13:35 +0100

    Add functionality of weekly todos and update styling

--- app.js
+++ app.js
@@ -26,52 +26,109 @@ app.use(express.static("public"));
 //   { id: 2, title: "Finish homework" },
 // ];
 
-async function getItems() {
-  const result = await db.query("SELECT *  FROM items ORDER BY id ASC");
+async function getItems(query) {
+  const result = await db.query(query);
   const items = result.rows;
   console.log(items);
   return items;
 }
 
+function pathname(req) {
+  const referer = req.get("Referer");
+  if (!referer) return "/";
+
+  try {
+    const url = new URL(referer);
+    return url.pathname;
+  } catch (error) {
+    console.error("Invalid Referer URL:", error);
+    return "/";
+  }
+}
+
 app.get("/", async (req, res) => {
-  const items = await getItems();
+  const items = await getItems("SELECT *  FROM daily_todos ORDER BY id ASC");
   res.render("index", {
     listTitle: "Today",
     listItems: items,
+    link: "/weekly",
+    linkText: "Weekly Todos"
+  });
+});
+
+app.get("/weekly", async (req, res) => {
+  const items = await getItems("SELECT * FROM weekly_todos ORDER BY id ASC");
+  res.render("weekly", {
+    listTitle: "This Week",
+    listItems: items,
+    link: "/",
+    linkText: "Daily Todos"
   });
 });
 
 app.post("/add", async (req, res) => {
+  const pathName = pathname(req);
+  
   const item = req.body.newItem;
-  try {
-    await db.query("INSERT INTO items (title) VALUES ($1)", [item]);
-    res.redirect("/");
-  } catch (error) {
-    console.error("Error inserting to database", error.stack);
+  if (pathName === "/") {
+    try {
+      await db.query("INSERT INTO daily_todos (title) VALUES ($1)", [item]);
+      res.redirect("/");
+    } catch (error) {
+      console.error("Error inserting to database", error.stack);
+    }
+  } else if (pathName === "/weekly") {
+    try {
+      await db.query("INSERT INTO weekly_todos (title) VALUES ($1)", [item]);
+      res.redirect("/weekly");
+    } catch (error) {
+      console.error("Error inserting to database", error.stack);
+    }
   }
 });
 
 app.post("/edit", async (req, res) => {
+  const pathName = pathname(req);
   const updatedItemId = req.body["updatedItemId"]
   const updatedItemTitle = req.body["updatedItemTitle"]
   console.log(updatedItemId, updatedItemTitle);
-  try {
-    await db.query("UPDATE items SET title = $1 WHERE id = $2", [updatedItemTitle, updatedItemId]);
-    res.redirect("/");
-  } catch (error) {
-    console.error("Error Updating items in database", error.stack);
+  if (pathName === "/") {
+    try {
+      await db.query("UPDATE daily_todos SET title = $1 WHERE id = $2", [updatedItemTitle, updatedItemId]);
+      res.redirect("/");
+    } catch (error) {
+      console.error("Error Updating items in database", error.stack);
+    }
+  } else if (pathName === "/weekly") {
+    try {
+      await db.query("UPDATE weekly_todos SET title = $1 WHERE id = $2", [updatedItemTitle, updatedItemId]);
+      res.redirect("/weekly");
+    } catch (error) {
+      console.error("Error Updating items in database", error.stack);
+    }
   }
 });
 
 app.post("/delete", async (req, res) => {
+  const pathName = pathname(req);
+
   const itemId = req.body["deleteItemId"];
-  try {
-    await db.query("DELETE FROM items WHERE id = $1", [itemId]);
-    res.redirect("/");
-  } catch (error) {
-    console.log("Error deleting from database", error.stack);
+
+  if (pathName === "/") {
+    try {
+      await db.query("DELETE FROM daily_todos WHERE id = $1", [itemId]);
+      res.redirect("/");
+    } catch (error) {
+      console.error("Error deleting from database", error.stack);
+    }
+  } else if (pathName === "/weekly") {
+    try {
+      await db.query("DELETE FROM weekly_todos WHERE id = $1", [itemId]);
+      res.redirect("/weekly");
+    } catch (error) {
+      console.error("Error deleting from database", error.stack);
+    }
   }
-  
 });
 
 app.listen(port, () => {
